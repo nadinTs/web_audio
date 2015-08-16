35b8cbf2066438dddbe57dbd3077a1c291dab8e6
Nadin Nadin <nadin.ts@gmail.com>
2015-08-16 13:44:40 +0300

    функции: play, stop, volume, загрузка файла и проигрывание, название файла

--- js/script.js
+++ js/script.js
@@ -1,40 +1,92 @@
+(function(){
+    var player = document.querySelector("#player");
+    var playButton = document.querySelector(".play");
+    var stopButton = document.querySelector(".stop");
+    var statusText = document.querySelector(".status");
+    var volumeSlider = document.querySelector('input');
+
+    var url = player.dataset['sound'];
+    var upload_list = document.querySelector('.upload_list');
+    var audio_item = document.createElement('p');
+
     var context = new AudioContext();
+    var source = context.createBufferSource();
+    var volume = context.createGain();
 
 
-    function loadAudio(object, url) {
-        var request = new XMLHttpRequest();
-        request.open('GET', url, true);
-        request.responseType = 'arraybuffer';
+    //var request = new XMLHttpRequest();
+    //request.open('GET', url, true);
+    //request.responseType = 'arraybuffer';
+    //
+    //request.onload = function () {
+    //    console.log('audio loaded');
+    //    statusText.innerHTML = 'Decoding audio, please wait...';
+    //    context.decodeAudioData(request.response, function (buffer) {
+    //        console.log('audio decoded.');
+    //        statusText.innerHTML = 'Click play to start.';
+    //        source.buffer = buffer;
+    //        source.connect(volume);
+    //        volume.connect(context.destination);
+    //    });
+    //};
+    //request.send();
+    //console.log('start loading audio from ' + url);
 
-        request.onload = function () {
-            context.decodeAudioData(request.response, function (buffer) {
-                object.buffer = buffer;
+    player.querySelector('#upload_audio').addEventListener('change', function(){
+       var files = this.files;
+
+        for (var i = 0; i < files.length; i++){
+            loadFile(files[i]);
+        }
+    });
+
+    function loadFile(file) {
+        if (file.type.match(/audio.*/ )) {
+            var reader = new FileReader();
+            console.log("start loading audio file " + file.name);
+
+            reader.addEventListener('load', function(event) {
+                console.log('audio file loaded, decoding...');
+                audio_item.title = file.name;
+                audio_item.innerHTML = file.name;
+                statusText.innerHTML = 'Decoding audio, please wait...';
+
+                context.decodeAudioData(event.target.result, function (buffer) {
+                    console.log('audio decoded.');
+
+                    statusText.innerHTML = 'Click play to start.';
+                    source.buffer = buffer;
+                    source.connect(volume);
+                    volume.connect(context.destination);
+                });
+
             });
-        };
-        request.send();
-    }
 
-    function addAudioProperties(object) {
-        object.name = object.id;
-        object.source = $(object).data('sound');
-        loadAudio(object, object.source);
-        object.play = function () {
-            var s = context.createBufferSource();
-            s.buffer = object.buffer;
-            s.connect(context.destination);
-            s.start(0);
-            object.s = s;
+            reader.readAsArrayBuffer(file);
+        }
+        else {
+            alert("это не музыкальный файл");
         }
     }
 
 
 
-$(function() {
-    $('section#sp div').each(function() {
-        addAudioProperties(this);
+    playButton.addEventListener("click", function(){
+        source.start(0);
     });
 
-    $('section#sp div').click(function() {
-        this.play();
+    stopButton.addEventListener("click", function(){
+        source.stop(0);
     });
-});
+
+    volumeSlider.addEventListener("change", function(){
+       console.log(volumeSlider.value);
+        volume.gain.value = volumeSlider.value;
+
+    });
+
+    audio_item.addEventListener("click", function(){
+       source.dataset['sound'] = this.dataset['sound'];
+    });
+
+})();
